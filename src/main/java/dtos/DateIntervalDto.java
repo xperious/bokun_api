802d1ad5531ab4286f7485fdd445ea9644de812d
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2012-09-20 18:40:56 +0200

    Added getters and setters.

--- src/main/java/dtos/DateIntervalDto.java
+++ src/main/java/dtos/DateIntervalDto.java
@@ -3,5 +3,23 @@ package dtos;
 public class DateIntervalDto {
 
 	public Long start;
+	public Long getStart() {
+		return start;
+	}
+
+	public void setStart(Long start) {
+		this.start = start;
+	}
+
+	public Long getEnd() {
+		return end;
+	}
+
+	public void setEnd(Long end) {
+		this.end = end;
+	}
+
 	public Long end;
+	
+	public DateIntervalDto() {}
 }
