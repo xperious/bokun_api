225ffe538598dcf55fc113aaacb6607eb963f191
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2012-10-21 21:54:53 +0000

    Added DTO for Activity availability.

---
+++ src/main/java/dtos/activity/ActivityAvailabilityDto.java
@@ -0,0 +1,13 @@
+package dtos.activity;
+
+import java.util.Date;
+
+public class ActivityAvailabilityDto {
+
+	public Long startTimeId;
+	public Date date;
+    public int availabilityCount;
+    public boolean unlimitedAvailability;
+    
+    public ActivityAvailabilityDto() {}
+}
