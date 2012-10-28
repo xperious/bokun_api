94af8e20d264cbc6a12eabb1781ce3bd3dad412f
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2012-10-28 20:24:33 +0000

    Added getters/setters to get the Play param binding working.

--- src/main/java/dtos/UserAccountDto.java
+++ src/main/java/dtos/UserAccountDto.java
@@ -11,4 +11,46 @@ public class UserAccountDto {
 	
 	@JsonIgnore
 	public String password;
+	
+	public UserAccountDto() {}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
