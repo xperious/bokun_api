00e6f3509595d6e93689cbc2d8b072d9c708e8e7
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2013-01-20 17:19:58 +0000

    Added methods to check if activity availability is unavailable or sold out

--- src/main/java/dtos/activity/ActivityAvailabilityDto.java
+++ src/main/java/dtos/activity/ActivityAvailabilityDto.java
@@ -4,12 +4,23 @@ import java.util.Date;
 
 public class ActivityAvailabilityDto {
 
+	public String id;
 	public String startTime;
 	public Long startTimeId;
 	public Date date;
 	public String localizedDate;
     public int availabilityCount;
     public boolean unlimitedAvailability;
+    public int adultPrice;
     
     public ActivityAvailabilityDto() {}
+
+    public boolean isUnavailable() {
+    	return !(availabilityCount > 0 || unlimitedAvailability == true);
+    }
+    
+    public boolean isSoldOut() {
+    	return unlimitedAvailability == false && availabilityCount == 0;
+    }
+
 }
