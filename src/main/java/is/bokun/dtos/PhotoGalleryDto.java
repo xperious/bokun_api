4d3586dcb4f89787d28a25870bd8f1750df9f3aa
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2013-05-23 00:07:28 +0000

    Removed unused Moo annotations.

--- src/main/java/is/bokun/dtos/PhotoGalleryDto.java
+++ src/main/java/is/bokun/dtos/PhotoGalleryDto.java
@@ -7,9 +7,7 @@ import com.codiform.moo.annotation.Property;
 
 public class PhotoGalleryDto {
 
-	@Property(translate = true)
 	public PhotoDto keyPhoto;
 	
-	@CollectionProperty(itemTranslation = PhotoDto.class, translation = "getPhotos()")
 	public List<PhotoDto> photos;
 }
--- src/main/java/is/bokun/dtos/VideoGalleryDto.java
+++ src/main/java/is/bokun/dtos/VideoGalleryDto.java
@@ -2,10 +2,7 @@ package is.bokun.dtos;
 
 import java.util.List;
 
-import com.codiform.moo.annotation.CollectionProperty;
-
 public class VideoGalleryDto {
 
-	@CollectionProperty(itemTranslation = VideoDto.class, translation = "getVideos()")
 	public List<VideoDto> videos;
 }
