904535d95acfecbcbc56dcf2ce27ffd75177eb4b
Ólafur Gauti Guðmundsson <oli.gauti@gmail.com>
2013-06-03 21:52:08 +0000

    Added query object for rental cars.

---
+++ src/main/java/is/bokun/queries/CarQuery.java
@@ -0,0 +1,175 @@
+package is.bokun.queries;
+
+import is.bokun.utils.StringUtils;
+
+import java.util.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ * Query object for searching rental cars.
+ *
+ * @author Olafur Gauti Gudmundsson
+ */
+public class CarQuery extends AbstractDateRangeQuery {
+	
+	/**
+	 * Set this to get results for specific car rentals only.
+	 */
+	public List<Long> carRentalIds = new ArrayList<>();
+	
+	/**
+	 * Set to get only cars that allow this driver age (and above). 
+	 */
+	public Integer driverAge;
+	
+	/**
+	 * Set to get only cars where passenger capacity is greater than or equal to this.
+	 */
+	public Integer passengers;
+	
+	/**
+	 * Set to get only cars where luggage capacity is greater than or equal to this.
+	 */
+	public Integer luggage;
+	
+	/**
+	 * Set to filter by CO2 emission (g/km).
+	 */
+	public NumericRangeFilter co2EmissionFilter;
+	
+	/**
+	 * Set to filter by fuel economy (L/100km).
+	 */
+	public NumericRangeFilter fuelEconomyFilter;
+	
+	/**
+	 * Set to filter by air conditioning.
+	 */
+	public Boolean airConditioning;
+
+    /**
+     * Find cars that have pickup location that matches this filter.
+     */
+	public LocationFilters pickupLocationFilters = new LocationFilters();
+	
+    /**
+     * Find cars that have dropoff location that matches this filter.
+     */
+	public LocationFilters dropoffLocationFilters = new LocationFilters();
+	
+	/**
+	 * Set this to get results for specific pickup locations only.
+	 */
+	public List<Long> pickupLocationIds = new ArrayList<>();
+	
+	/**
+	 * Set this to get results for specific dropoff locations only.
+	 */
+	public List<Long> dropoffLocationIds = new ArrayList<>();
+
+	public CarQuery() {
+		super();
+	}
+
+	public List<Long> getCarRentalIds() {
+		return carRentalIds;
+	}
+
+	public void setCarRentalIds(List<Long> carRentalIds) {
+		this.carRentalIds = carRentalIds;
+	}
+
+	public Integer getDriverAge() {
+		return driverAge;
+	}
+
+	public void setDriverAge(Integer driverAge) {
+		this.driverAge = driverAge;
+	}
+
+	public Integer getPassengers() {
+		return passengers;
+	}
+
+	public void setPassengers(Integer passengers) {
+		this.passengers = passengers;
+	}
+
+	public Integer getLuggage() {
+		return luggage;
+	}
+
+	public void setLuggage(Integer luggage) {
+		this.luggage = luggage;
+	}
+
+	public LocationFilters getPickupLocationFilters() {
+		return pickupLocationFilters;
+	}
+
+	public void setPickupLocationFilters(LocationFilters pickupLocationFilters) {
+		this.pickupLocationFilters = pickupLocationFilters;
+	}
+
+	public LocationFilters getDropoffLocationFilters() {
+		return dropoffLocationFilters;
+	}
+
+	public void setDropoffLocationFilters(LocationFilters dropoffLocationFilters) {
+		this.dropoffLocationFilters = dropoffLocationFilters;
+	}
+
+	public List<Long> getPickupLocationIds() {
+		return pickupLocationIds;
+	}
+
+	public void setPickupLocationIds(List<Long> pickupLocationIds) {
+		this.pickupLocationIds = pickupLocationIds;
+	}
+
+	public List<Long> getDropoffLocationIds() {
+		return dropoffLocationIds;
+	}
+
+	public void setDropoffLocationIds(List<Long> dropoffLocationIds) {
+		this.dropoffLocationIds = dropoffLocationIds;
+	}
+
+	public NumericRangeFilter getCo2EmissionFilter() {
+		return co2EmissionFilter;
+	}
+
+	public void setCo2EmissionFilter(NumericRangeFilter co2EmissionFilter) {
+		this.co2EmissionFilter = co2EmissionFilter;
+	}
+
+	public NumericRangeFilter getFuelEconomyFilter() {
+		return fuelEconomyFilter;
+	}
+
+	public void setFuelEconomyFilter(NumericRangeFilter fuelEconomyFilter) {
+		this.fuelEconomyFilter = fuelEconomyFilter;
+	}
+
+	public Boolean getAirConditioning() {
+		return airConditioning;
+	}
+
+	public void setAirConditioning(Boolean airConditioning) {
+		this.airConditioning = airConditioning;
+	}
+	
+	@JsonIgnore
+	public CarSortField sortField() {
+		if ( StringUtils.isNullOrEmpty(sortField) ) {
+			return null;
+		} else {
+			try {
+				return CarSortField.valueOf(sortField.toUpperCase());
+			} catch ( Throwable ignored ) {
+				return null;
+			}
+		}
+	}
+}
---
+++ src/main/java/is/bokun/queries/CarSortField.java
@@ -0,0 +1,29 @@
+package is.bokun.queries;
+
+/**
+ * The available sort fields for Rental Car results.
+ *
+ * @author Olafur Gauti Gudmundsson
+ */
+public enum CarSortField {
+
+	BEST_SELLING_GLOBAL,
+	BEST_SELLING_IN_CHANNEL,
+	PRICE,
+	DISTANCE,
+	ALPHABETIC,
+	RANDOM;
+	
+	public String field() {
+		switch (this) {
+			case BEST_SELLING_GLOBAL:
+				return "booking_count";
+			case BEST_SELLING_IN_CHANNEL:
+				return "booking_count_channel_";
+			case PRICE:
+				return "totalPrice";
+			default:
+				return "_score";
+		}
+	}
+}
