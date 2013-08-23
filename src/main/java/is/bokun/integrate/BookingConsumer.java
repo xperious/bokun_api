fed409e71633ea80b3ad3b4cdde1e05688ee6951
Ólafur Gauti Guðmundsson <ogg@bokun.is>
2013-08-23 14:02:49 +0000

    Consume BookingDetailsDto

--- src/main/java/is/bokun/integrate/BookingConsumer.java
+++ src/main/java/is/bokun/integrate/BookingConsumer.java
@@ -1,6 +1,8 @@
 package is.bokun.integrate;
 
+import is.bokun.dtos.booking.BookingDetailsDto;
+
 public interface BookingConsumer {
 
-	public boolean consume();
+	public boolean consume(BookingDetailsDto booking);
 }
