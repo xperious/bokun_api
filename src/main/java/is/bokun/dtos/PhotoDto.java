8ea7badee680d98bd66fbfebd599b32640056244
Ólafur Gauti Guðmundsson <ogg@bokun.is>
2013-10-18 18:51:51 +0000

    Added translateable description and alternate text to photos. Also added flags. Made all editable via UI.

--- src/main/java/is/bokun/dtos/PhotoDto.java
+++ src/main/java/is/bokun/dtos/PhotoDto.java
@@ -13,11 +13,52 @@ public class PhotoDto {
 
 	public Long id;
     public String originalUrl;
+    
+	public String description;
+	public String alternateText;
+	
+	public List<String> flags = new ArrayList<>();
 
 	public List<DerivedPhotoDto> derived = new ArrayList<>();
 	
 	public PhotoDto() {}
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOriginalUrl() {
+		return originalUrl;
+	}
+	public void setOriginalUrl(String originalUrl) {
+		this.originalUrl = originalUrl;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAlternateText() {
+		return alternateText;
+	}
+	public void setAlternateText(String alternateText) {
+		this.alternateText = alternateText;
+	}
+	public List<String> getFlags() {
+		return flags;
+	}
+	public void setFlags(List<String> flags) {
+		this.flags = flags;
+	}
+	public List<DerivedPhotoDto> getDerived() {
+		return derived;
+	}
+	public void setDerived(List<DerivedPhotoDto> derived) {
+		this.derived = derived;
+	}
 	@JsonIgnore
 	public DerivedPhotoDto findDerived(String name) {
 		for (DerivedPhotoDto d : derived) {
