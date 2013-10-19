d8cb629dbb1fd9add801dde588f8b99773cdbd7f
Ólafur Gauti Guðmundsson <ogg@bokun.is>
2013-10-19 00:13:01 +0000

    Added ability to provide extra answers when adding to shopping cart.

--- src/main/java/is/bokun/dtos/booking/ExtraBookingRequestDto.java
+++ src/main/java/is/bokun/dtos/booking/ExtraBookingRequestDto.java
@@ -1,12 +1,16 @@
 package is.bokun.dtos.booking;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.*;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtraBookingRequestDto {
 
 	public Long extraId;
 	public int unitCount;
+	
+	public List<BookingAnswerWithIdGroupDto> answers = new ArrayList<BookingAnswerWithIdGroupDto>();
 	
 	public ExtraBookingRequestDto() {}
 
@@ -25,4 +29,13 @@ public class ExtraBookingRequestDto {
 	public void setUnitCount(int unitCount) {
 		this.unitCount = unitCount;
 	}
+
+	public List<BookingAnswerWithIdGroupDto> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<BookingAnswerWithIdGroupDto> answers) {
+		this.answers = answers;
+	}
+
 }
