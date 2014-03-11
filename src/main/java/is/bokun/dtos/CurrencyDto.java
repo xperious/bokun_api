295ceaa0bd3124a74a67d3730d082eecb2e60f55
Ólafur Gauti Guðmundsson <ogg@bokun.is>
2014-03-11 00:55:47 +0000

    Currency cleanup.

--- src/main/java/is/bokun/dtos/CurrencyDto.java
+++ src/main/java/is/bokun/dtos/CurrencyDto.java
@@ -5,13 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrencyDto {
 
-	public Long id;
-	public boolean base;
+	public boolean payment;
     public String code;
     
     public Double rate;
-    
-    public String sign;
     
     public CurrencyDto() {}
 }
