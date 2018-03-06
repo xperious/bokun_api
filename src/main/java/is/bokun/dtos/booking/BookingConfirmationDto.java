3ae8ef438a86d0b19562a6fb420abb64d70fe924
Dmitrij Abolichin <dmitrij@tripcreator.com>
2018-03-06 10:55:03 +0200

    Add externalBookingReference

--- src/main/java/is/bokun/dtos/booking/BookingConfirmationDto.java
+++ src/main/java/is/bokun/dtos/booking/BookingConfirmationDto.java
@@ -9,6 +9,8 @@ public class BookingConfirmationDto {
 
 //	public BookingPaymentInfoDto payment;
 
+	public String externalBookingReference;
+
 	public CustomerPaymentDto payment;
 
 	public List<BookingFieldDto> bookingFields = new ArrayList<>();
@@ -36,5 +38,13 @@ public class BookingConfirmationDto {
 
 	public void setBookingFields(List<BookingFieldDto> bookingFields) {
 		this.bookingFields = bookingFields;
+	}
+
+	public String getExternalBookingReference() {
+		return externalBookingReference;
+	}
+
+	public void setExternalBookingReference(String externalBookingReference) {
+		this.externalBookingReference = externalBookingReference;
 	}
 }
